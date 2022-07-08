f8c4fd8da1d1d09af13030ddf45c610b31c6ce0a
Fancinnov <JackyPan@fancinnov.com>
2022-07-08 15:21:19 +0800

    update position control

--- Cpplibrary/include/position/position.h
+++ Cpplibrary/include/position/position.h
@@ -37,9 +37,9 @@
 #define POSCONTROL_ACTIVE_TIMEOUT_MS            200     // position controller is considered active if it has been called within the past 0.2 seconds
 
 #define POSCONTROL_VEL_TARGET_CUTOFF_FREQ       20.0f    // low-pass filter on velocity error (unit: hz)
-#define POSCONTROL_VEL_ERROR_CUTOFF_FREQ        1.0f    // low-pass filter on velocity error (unit: hz)
-#define POSCONTROL_THROTTLE_CUTOFF_FREQ         1.0f    // low-pass filter on accel error (unit: hz)
-#define POSCONTROL_ACCEL_FILTER_HZ              1.0f    // low-pass filter on acceleration (unit: hz)
+#define POSCONTROL_VEL_ERROR_CUTOFF_FREQ        4.0f    // low-pass filter on velocity error (unit: hz)
+#define POSCONTROL_THROTTLE_CUTOFF_FREQ         2.0f    // low-pass filter on accel error (unit: hz)
+#define POSCONTROL_ACCEL_FILTER_HZ              2.0f    // low-pass filter on acceleration (unit: hz)
 #define POSCONTROL_JERK_RATIO                   1.0f    // Defines the time it takes to reach the requested acceleration
 
 #define POSCONTROL_OVERSPEED_GAIN_Z             2.0f    // gain controlling rate at which z-axis speed is brought back within SPEED_UP and SPEED_DOWN range
